9e519d75928240da19aabbdda7a6f7886ef1a2a9
JoWatson2011 <jowatson@protonmail.com>
2024-12-19 15:45:27 +0000

    Day 3 part 2

--- aoc-2024/aoc-2024-day-3.js
+++ aoc-2024/aoc-2024-day-3.js
@@ -4,19 +4,12 @@ const { readFile } = promises;
 const solve = async (path) => {
   const data = await readFile(path, "utf-8");
 
-  const corruptedInstructions = data.split("\n").slice(0, 1);
-
-  return sumCorruptedInstructions(corruptedInstructions);
-};
-
-const sumCorruptedInstructions = (corruptedInstructions) => {
-  return corruptedInstructions.reduce((total, corruptedInstruction) => {
-    const numbersPaired = cleanUpInstruction(corruptedInstruction);
-    const numsMultiplied = numbersPaired.map(
-      (numberPairs) => numberPairs[0] * numberPairs[1]
-    );
-    return total + numsMultiplied.reduce((sum, num) => sum + num, 0);
-  }, 0);
+  const corruptedInstructions = data;
+  const numbersPaired = cleanUpInstruction(corruptedInstructions);
+  const numsMultiplied = numbersPaired.map(
+    (numberPairs) => numberPairs[0] * numberPairs[1]
+  );
+  return numsMultiplied.reduce((sum, num) => sum + num, 0);
 };
 
 const cleanUpInstruction = (corruptedInstruction) => {
@@ -26,12 +19,12 @@ const cleanUpInstruction = (corruptedInstruction) => {
   const enabledInstructions = [];
 
   for (let i = 0; i < splitInstruction.length; i++) {
-    if (i === 0) enabledInstructions.push(splitInstruction[i]);
+    if (i === 0) enabledInstructions.push(splitInstruction[0]);
     else if (/do\(\)/.test(splitInstruction[i])) {
-      const splitDisabled = splitInstruction[i].substring(
+      const splitEnabled = splitInstruction[i].slice(
         splitInstruction[i].indexOf("do()")
       );
-      enabledInstructions.push(splitDisabled);
+      enabledInstructions.push(splitEnabled);
     }
   }
 
