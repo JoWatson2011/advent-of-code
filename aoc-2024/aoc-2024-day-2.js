22cd4f1ad121db33875e002e0c126b1a091335ac
JoWatson2011 <jowatson@protonmail.com>
2024-12-19 11:55:26 +0000

    Part 2 solution

--- aoc-2024/aoc-2024-day-2.js
+++ aoc-2024/aoc-2024-day-2.js
@@ -4,53 +4,57 @@ const { readFile } = promises;
 const checkReports = async (path) => {
   const data = await readFile(path, "utf-8");
 
-  const reports = data.split("\n").map((report) => report.split(" "));
+  const reports = data
+    .split("\n")
+    .map((report) => report.split(" ").map((level) => Number(level)));
 
-  const part1Count = reports.reduce((safeReportCounter, report) => {
+  const count = reports.reduce((safeReportCounter, report) => {
     return checkLevels(report) ? safeReportCounter + 1 : safeReportCounter;
   }, 0);
 
-  const part2Count = reports.reduce((safeReportCounter, report) => {
-    const newReport = switchUnsafeLevels(report);
-    return checkLevels(newReport) ? safeReportCounter + 1 : safeReportCounter;
-  }, 0);
-
-  return { part1: part1Count, part2: part2Count };
+  return count;
 };
 
 const checkLevels = (report) => {
-  if (!allDecreasing(report) && !allIncreasing(report)) {
+  const isValid = (first, second, allIncreasing, allDecreasing) => {
+    const isIncreasing = first < second && allIncreasing;
+    const isDecreasing = first > second && allDecreasing;
+    const difference = first - second;
+
+    return (
+      Math.abs(difference) <= 3 &&
+      Math.abs(difference) >= 1 &&
+      (isDecreasing || isIncreasing)
+    );
+  };
+
+  const areSafe = report.slice(0, report.length - 1).map((level, index) => {
+    if (
+      isValid(
+        level,
+        report[index + 1],
+        report[0] < report[1],
+        report[0] > report[1]
+      )
+    )
+      return true;
     return false;
-  }
-
-  const areSafe = report.map((level, index) => {
-    if (index === 0) return true;
-
-    const difference = report[index - 1] - level;
-
-    if (Math.abs(difference) > 3 || Math.abs(difference) < 1) return false;
-
-    return true;
   });
 
-  return areSafe.every((level) => level);
-};
-
-const allIncreasing = (report) => {
-  return report.slice(0, report.length - 1).every((level, index) => {
-    return level < report[index + 1];
+  const areSomeSafe = report.some((_, index) => {
+    const splicedReport = report.toSpliced(index, 1);
+    return splicedReport
+      .slice(0, splicedReport.length - 1)
+      .map((level, i) =>
+        isValid(
+          level,
+          splicedReport[i + 1],
+          splicedReport[0] < splicedReport[1],
+          splicedReport[0] > splicedReport[1]
+        )
+      )
+      .every((ele) => ele);
   });
+  console.log(areSomeSafe);
+  return areSafe.every((level) => level) || areSomeSafe;
 };
-const allDecreasing = (report) => {
-  return report.slice(0, report.length - 1).every((level, index) => {
-    return level > report[index + 1];
-  });
-};
-
-const switchUnsafeLevels = (report) => {
-  const newReport = [];
-
-  return newReport.length ? newReport : report;
-};
-
-console.log(await checkReports("day-2-input-test.txt"));
